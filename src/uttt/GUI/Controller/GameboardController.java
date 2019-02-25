aacaef89e3533ac4d1de4d2ce0b645837f36c8ae
PhilipVJ <44639834+PhilipVJ@users.noreply.github.com>
2019-02-25 15:37:35 +0100

    coordinateFix

--- src/uttt/GUI/Controller/GameboardController.java
+++ src/uttt/GUI/Controller/GameboardController.java
@@ -68,12 +68,9 @@ public class GameboardController implements Initializable
     private void buttonPressed(ActionEvent event)
     {
         Button btn = (Button) event.getSource();
-        Integer row = macroBoard.getRowIndex(macroBoard);
-        Integer col = macroBoard.getColumnIndex(macroBoard);
-        System.out.println(""+row);
-        System.out.println(""+col);
+        Integer[] coordinates = makeCoordinates(btn);
 
-        if (gManager.updateGame(new Move(row,col)) == true)
+        if (gManager.updateGame(new Move(coordinates[0], coordinates[1])) == true)
         {
             if (currentPlayer == 0)
             {
@@ -102,6 +99,33 @@ public class GameboardController implements Initializable
             case 2:
             case 3:
         }
+    }
+
+    private Integer[] makeCoordinates(Button btn)
+    {
+        Integer row = GridPane.getRowIndex((Node) btn);
+        Integer col = GridPane.getColumnIndex((Node) btn);
+        
+        GridPane current = (GridPane) btn.getParent();
+        Integer row2 = GridPane.getRowIndex((Node) current);
+        Integer col2 = GridPane.getColumnIndex((Node) current);
+        
+        // local 3x3
+        int r = (row == null) ? 0 : row;
+        int c = (col == null) ? 0 : col;
+        // macro
+        int r1 = (row2 == null) ? 0 : row2;
+        int c1 = (col2 == null) ? 0 : col2;
+        
+       Integer[] coordinates = new Integer[2];
+       
+       coordinates[0]=r+(r1*3);
+       coordinates[1]=c+(c1*3);
+       
+        System.out.println(""+coordinates[0]);
+        System.out.println(""+coordinates[1]);
+       
+        return coordinates;
     }
 
 }
