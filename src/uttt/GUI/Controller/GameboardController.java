ac326fb67ef154d3940335e36d1b2064ff6fe9d8
Neller1989 <43983250+Neller1989@users.noreply.github.com>
2019-02-25 11:05:21 +0100

    midterrække

--- src/uttt/GUI/Controller/GameboardController.java
+++ src/uttt/GUI/Controller/GameboardController.java
@@ -7,7 +7,10 @@ package uttt.GUI.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 
 /**
  * FXML Controller class
@@ -16,6 +19,81 @@ import javafx.fxml.Initializable;
  */
 public class GameboardController implements Initializable
 {
+
+    @FXML
+    private GridPane macroBoard;
+    @FXML
+    private GridPane gridPane1;
+    @FXML
+    private GridPane gridPane2;
+    @FXML
+    private GridPane gridPane3;
+    @FXML
+    private GridPane gridPane4;
+    @FXML
+    private Button btn41;
+    @FXML
+    private Button btn42;
+    @FXML
+    private Button btn44;
+    @FXML
+    private Button btn45;
+    @FXML
+    private Button btn47;
+    @FXML
+    private Button btn48;
+    @FXML
+    private Button btn49;
+    @FXML
+    private Button btn46;
+    @FXML
+    private Button btn43;
+    @FXML
+    private GridPane gridPane5;
+    @FXML
+    private Button btn52;
+    @FXML
+    private Button btn51;
+    @FXML
+    private Button btn54;
+    @FXML
+    private Button btn55;
+    @FXML
+    private Button btn57;
+    @FXML
+    private Button btn58;
+    @FXML
+    private Button btn59;
+    @FXML
+    private Button btn56;
+    @FXML
+    private Button btn53;
+    @FXML
+    private GridPane gridPane6;
+    @FXML
+    private Button btn61;
+    @FXML
+    private Button btn62;
+    @FXML
+    private Button btn64;
+    @FXML
+    private Button btn65;
+    @FXML
+    private Button btn63;
+    @FXML
+    private Button btn66;
+    @FXML
+    private Button btn67;
+    @FXML
+    private Button btn68;
+    @FXML
+    private Button btn69;
+    @FXML
+    private GridPane gridPane7;
+    @FXML
+    private GridPane gridPane8;
+    @FXML
+    private GridPane gridPane9;
 
     /**
      * Initializes the controller class.
