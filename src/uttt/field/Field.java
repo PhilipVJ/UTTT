e8f9a490501eaa8af805dae76736914ccd96e319
Neller1989 <43983250+Neller1989@users.noreply.github.com>
2019-02-26 11:42:45 +0100

    update på microboardcoordinates

--- src/uttt/field/Field.java
+++ src/uttt/field/Field.java
@@ -143,18 +143,26 @@ public class Field implements IField
 
     public void setMicroboardCoordinates()
     {
-        String[] m1 = {"0.0", "0.1", "0.2", "1.0", "1.1", "1.2", "2.0","2.1","2.2"};
-        String[] m2 = new String[9];
-        String[] m3 = new String[9];
-        String[] m4 = new String[9];
-        String[] m5 = new String[9];
-        String[] m6 = new String[9];
-        String[] m7 = new String[9];
-        String[] m8 = new String[9];
-        String[] m9 = new String[9];
+        String[] m1 = {"0.0", "0.1", "0.2", "1.0", "1.1", "1.2", "2.0", "2.1", "2.2"};
+        String[] m2 = {"0.3", "0.4", "0.5", "1.3", "1.4", "1.5", "2.3", "2.4", "2.5"};
+        String[] m3 = {"0.6", "0.7", "0.8", "1.6", "1.7", "1.8", "2.6", "2.7", "2.8"};
+        String[] m4 = {"3.0", "3.1", "3.2", "4.0", "4.1", "4.2", "5.0", "5.1", "5.2"};
+        String[] m5 = {"3.3", "3.4", "3.5", "4.3", "4.4", "4.5", "5.3", "5.4", "5.5"};
+        String[] m6 = {"3.6", "3.7", "3.8", "4.6", "4.7", "4.8", "5.6", "5.7", "5.8"};
+        String[] m7 = {"6.0", "6.1", "6.2", "7.0", "7.1", "7.2", "8.0", "8.1", "8.2"};
+        String[] m8 = {"6.3", "6.4", "6.5", "7.3", "7.4", "7.5", "8.3", "8.4", "8.5"};
+        String[] m9 = {"6.6", "6.7", "6.8", "7.6", "7.7", "7.8", "8.6", "8.7", "8.8"};
         
         
-//        microBoards.add(e)
+        microBoards.add(m1);
+        microBoards.add(m2);
+        microBoards.add(m3);
+        microBoards.add(m4);
+        microBoards.add(m5);
+        microBoards.add(m6);
+        microBoards.add(m7);
+        microBoards.add(m8);
+        microBoards.add(m9);
     }
 
 }
